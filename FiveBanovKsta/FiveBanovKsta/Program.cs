54998d68361e5c73e61e0b05dc3415089ae1460b
Lisitsin <113295347+lisenok-lisitsin@users.noreply.github.com>
2022-09-28 23:59:11 +0300

    Add files via upload

--- FiveBanovKsta/FiveBanovKsta/Program.cs
+++ FiveBanovKsta/FiveBanovKsta/Program.cs
@@ -19,10 +19,10 @@ namespace FiveBanovKsta
             min = Convert.ToInt32(s);
             s = Console.ReadLine();
             max = Convert.ToInt32(s);
-            x1 = r.Next(min, max+1);
-            x2 = r.Next(min, max+1);
-            y1 = r.Next(min, max+1);
-            y2 = r.Next(min, max+1);
+            x1 = r.Next(min, max);
+            x2 = r.Next(min, max);
+            y1 = r.Next(min, max);
+            y2 = r.Next(min, max);
             l = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
             Console.WriteLine("X1 = {0}, X2 = {1}, Y1 = {2}, Y2 = {3}", x1, x2, y1, y2);
             Console.WriteLine("L = {0}", l);
