8586da85a64163cbbcfc846cf22dee0d5ed73cb8
Lisitsin <113295347+lisenok-lisitsin@users.noreply.github.com>
2023-05-22 12:37:53 +0300

    Add files via upload

---
+++ TableStudPrek/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TableStudPrek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bacon_Click(object sender, EventArgs e)
+        {
+            int rowco = Convert.ToInt32(rowc.Text);
+            int colco = Convert.ToInt32(colc.Text);
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int c = 29;
+            string[] b = RandomName.SortRName(c);
+            int er = DGV1.ColumnCount;
+            int jey = DGV1.Rows.Count;
+            int lmg = er + jey;
+            //string[] g = RandomMark.Default(c);
+            //string[] ea = RandomMark.Special(c);
+            string g;
+            for (int i = 0; i < c; i++)
+            {
+                g = RandomMark.Default(lmg);
+                DGV1.Rows.Add(b[i]);
+                //DGV1.Rows[i].Cells[3].Value = ea[i];
+                //DGV1.Rows[i].Cells[1].Value = g[i];
+                DGV1.Rows[i].Cells[1].Value = g;
+                DGV1.Rows[i].HeaderCell.Value = $"{i + 1}";
+            }
+
+            
+
+            
+            //string g;
+            //for (int i = 0; i < lmg; i++)
+            //{
+            //    DGV1.Rows[i].Cells[i+1].Value = g;
+            //}
+            DGV1.TopLeftHeaderCell.Value = "Номер";
+            
+        }
+    }
+}
---
+++ TableStudPrek/RandomMark.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace TableStudPrek
+{
+    public class RandomMark
+    {
+        
+        private static string[] MarksDEF = { "Н/А", "2", "3", "4", "5" };
+        private static string[] MarksSPE = { "не зачёт", "зачёт", "Н/А" };
+
+        public static string[] totalD;
+        public static string[] totalSP;
+
+        public static string totalDS;
+
+        public static string[] Default(int num1)
+        {
+            Random r = new Random();
+            totalD = new string[num1];
+            int temp;
+
+            for (int  i = 0; i < num1; i++)
+            {
+                temp = r.Next(0, 101);
+                if (temp <= 25)
+                {
+                    totalD[i] = MarksDEF[0];
+                }
+                else if (temp > 25 && temp <= 50)
+                {
+                    totalD[i] = MarksDEF[1];
+                }
+                else if (temp > 50 &&  temp <= 65)
+                {
+                    totalD[i] = MarksDEF[2];
+                }
+                else if (temp > 65 && temp <= 75)
+                {
+                    totalD[i] = MarksDEF[3];
+                }
+                else if (temp > 75)
+                {
+                    totalD[i] = MarksDEF[4];
+                }
+            }
+            return totalD;
+        }
+
+        public static string DefaultStandalone()
+        {
+            Random r = new Random();
+            int temp;
+
+            temp = r.Next(0, 101);
+            if (temp <= 25)
+            {
+                totalDS = MarksDEF[0];
+            }
+            else if (temp > 25 && temp <= 50)
+            {
+                totalDS = MarksDEF[1];
+            }
+            else if (temp > 50 && temp <= 65)
+            {
+                totalDS = MarksDEF[2];
+            }
+            else if (temp > 65 && temp <= 75)
+            {
+                totalDS = MarksDEF[3];
+            }
+            else if (temp > 75)
+            {
+                totalDS = MarksDEF[4];
+            }
+
+            return totalDS;
+        }
+
+        public static string[] Special(int num1)
+        {
+            Random r = new Random();
+            totalSP = new string[num1];
+            int temp;
+
+            for (int i = 0; i < num1; i++)
+            {
+                temp = r.Next(0, 101);
+                if (temp <= 25)
+                {
+                    totalSP[i] = MarksSPE[2];
+                }
+                else if (temp > 25 && temp < 50)
+                {
+                    totalSP[i] = MarksSPE[0];
+                }
+                else if (temp >= 50)
+                {
+                    totalSP[i] = MarksSPE[1];
+                }
+            }
+            return totalSP;
+        }
+
+    }
+}
