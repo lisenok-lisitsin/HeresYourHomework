00b2177121ae4735867df0d2c6d90f310f3ec0f2
Lisitsin <113295347+lisenok-lisitsin@users.noreply.github.com>
2023-05-29 13:06:29 +0300

    Add files via upload

---
+++ HeresYourHomework-ClassIC/TableStudPrek/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TableStudPrek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bacon_Click(object sender, EventArgs e)
+        {
+            int rowco = Convert.ToInt32(rowc.Text);
+            int colco = Convert.ToInt32(colc.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int c = 29;
+            string[] b = RandomName.SortRName(c);
+            int er = DGV1.ColumnCount;
+            int lmg = (er * c);
+            string[] fr = RandomMark.Default(lmg);
+            DGV1.Rows.Add(c-1);
+            int jey = DGV1.Rows.Count;
+            for (int i = 0; i < er; i++)
+            {
+                for (int j = 0; j < jey; j++)
+                {
+                    DGV1.Rows[j].Cells[i].Value = fr[k];
+                }
+            }
+            for (int i = 0; i < c; i++)
+            {
+                DGV1.Rows[i].Cells[0].Value = (b[i]);
+                DGV1.Rows[i].HeaderCell.Value = $"{i + 1}";
+            }
+            DGV1.TopLeftHeaderCell.Value = "Номер";
+        }
+    }
+}
---
+++ HeresYourHomework-ClassIC/TableStudPrek/RandomName.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableStudPrek
+{
+    public class RandomName
+    {
+        private static Random random = new Random();
+
+        private static string[] firstNames = { "Иван", "Родион", "Святослав", "Фёдор", "Михаил", "Максим", "Сергей", "Артём", "Андрей", "Владимир", "Ярослав", "Валентин", "Геннадий", "Игорь", "Олег", "Руслан", "Герасим", "Ярополк", "Бекмек" };
+        private static string[] lastNames = { "Рахматов", "Пражин", "Прядко", "Филипов", "Васильев", "Эсмиральдов", "Кац", "Демошенко", "Пономарёв", "Кравченко", "Ягудин", "Маршаков", "Славянских" };
+        private static string[] midNames = { "И.", "В.", "Б.", "Г.", "А.", "С.", "Р." };
+
+        private static string[] firstNamesF = { "Жанна", "Констанция", "Екатерина", "Анастасия", "Марьяна", "Мария", "Александра", "Евгения", "Ирина", "Елена", "Светлана", "Катерина" };
+        private static string[] lastNamesF = { "Ибрагимова", "Сергеева", "Тролл", "Шпицманн", "Велинская", "Рыбникова" };
+        private static string[] midNamesF = { "С.", "Д.", "Е.", "Г.", "М.", "И.", "К.", "Т.", "У.", "В." };
+
+        private static string[] sorted;
+
+        public static string GenFName()
+        {
+            string firstName = firstNames[random.Next(firstNames.Length)];
+            string lastName = lastNames[random.Next(lastNames.Length)];
+            string midName = midNames[random.Next(midNames.Length)];
+            return $"{lastName} {firstName} {midName}";
+        }
+
+        public static string GetFNameF()
+        {
+            string firstNameF = firstNamesF[random.Next(firstNamesF.Length)];
+            string lastNameF = lastNamesF[random.Next(lastNamesF.Length)];
+            string midNameF = midNamesF[random.Next(midNamesF.Length)];
+            return $"{lastNameF} {firstNameF} {midNameF}";
+        }
+
+        public static string[] SortRName(int num1)
+        {
+            Random r = new Random();
+            sorted = new string[num1];
+            int temp;
+            for (int i = 0; i < num1; i++)
+            {
+                temp = r.Next(0, 101);
+                if (temp <= 75)
+                {
+                    string g = RandomName.GenFName();
+                    sorted[i] = g;
+                }
+                if (temp >= 76)
+                {
+                    string g = RandomName.GetFNameF();
+                    sorted[i] = g;
+                }
+            }
+            Array.Sort(sorted);
+            return sorted;
+        }
+    }
+}
